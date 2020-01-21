43f8cd7f87108bd0adfa1fcab39dfaa9663c2bf1
David Smith <david.smith@weaveability.com>
2020-01-21 15:24:33 +0000

    implement navigation styling

--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,20 @@
-import PropTypes from 'prop-types'
-import React from 'react'
+import PropTypes from "prop-types";
+import React from "react";
+import styleable from "react-styleable";
+
+import css from "./nav.module.css";
 
 function Nav(props) {
   return (
-    <div>
-      <button onClick={props.onPrevious}>&#10094;</button>
-      <button onClick={props.onNext}>&#10095;</button>
+    <div className={props.css.root}>
+      <button className={props.css.prev} onClick={props.onPrevious}>
+        &#10094;
+      </button>
+      <button className={props.css.next} onClick={props.onNext}>
+        &#10095;
+      </button>
     </div>
-  )
+  );
 }
 
 Nav.propTypes = {
@@ -15,6 +22,6 @@ Nav.propTypes = {
   onNext: PropTypes.func.isRequired,
   hasPrevious: PropTypes.bool,
   hasNext: PropTypes.bool
-}
+};
 
-export default Nav
+export default styleable(css)(Nav);
